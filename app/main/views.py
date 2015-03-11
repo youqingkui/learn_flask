8c0b6f667221d008d3586210bbc88dbeb1e952b7
youqingkui <youqingkui@qq.com>
2015-03-11 17:56:36 +0800

    test chapter 8

---
+++ app/main/views.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from datetime import datetime
+from flask import render_template, session, redirect, url_for, current_app
+
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+from ..email import send_email
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            print "hi"
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            print "to"
+            session['known'] = True
+
+        session['name'] = form.name.data
+        return redirect(url_for('main.index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known'))
